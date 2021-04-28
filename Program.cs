4f9937d00555da7ed2504bf795a67ec729fbdff6
Greg <gregmt99@gmail.com>
2021-04-28 18:03:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawl(120, DateTime.Now, "Hammock");
+            Console.WriteLine($"account balance is - {account.Balance}");
+        }
+    }
+}
